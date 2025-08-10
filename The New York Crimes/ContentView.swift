5925f5e0e96c2821267d4bb0495c4014b6acbbd5
Oliver Tran <olivertransf@gmail.com>
2025-08-09 18:21:36 -0700

    Initial Commit

---
+++ The New York Crimes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The New York Crimes
+//
+//  Created by Oliver Tran on 8/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The New York Crimes/The_New_York_CrimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_New_York_CrimesApp.swift
+//  The New York Crimes
+//
+//  Created by Oliver Tran on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct The_New_York_CrimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
